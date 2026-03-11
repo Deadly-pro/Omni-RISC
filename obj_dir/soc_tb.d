8e0ecdc6fb5018306a2687dd038e70fcc896e7f5
Prajwal R <ruyketsu.prajwal@gmail.com>
2026-03-11 16:04:33 +0530

    Updated code base
    removed un necessary obj_dir files to push for git

--- obj_dir/soc_tb.d
+++
@@ -1,9 +0,0 @@
-soc_tb.o: ../hardware/sim/soc_tb.cpp \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilated_types.h \
- /usr/share/verilator/include/verilated_funcs.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h Vsoc_top.h
